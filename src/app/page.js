014c6c8bbbf5c94a523d7707db7948670c4272f0
Mateus <mateusfpmelodev@gmail.com>
2024-07-29 16:45:36 -0300

    acerto css, inserção de marca, adaptação mobile e multiplataforma

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
 'use client'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Tooltip } from 'bootstrap'
 import Image from 'next/image'
 import styles from './page.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { PiInfoFill } from "react-icons/pi";
+
+
 
 export default function Home() {
   const [subject, setSubject] = useState('')
@@ -10,9 +16,14 @@ export default function Home() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    // Aqui você pode adicionar a lógica para manipular o submit do formulário
+
     console.log({ subject, description, email })
   }
+
+  useEffect(() => {
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.map((tooltipTriggerEl) => new Tooltip(tooltipTriggerEl))
+  }, [])
 
   return (
     <main className='basepage'>
@@ -20,14 +31,18 @@ export default function Home() {
         <div className={styles.main}>
         </div>
         <div className={styles.mainform}>
-          <h1>Ouvidoria</h1>
+          <Image src={'/marca/sy_w_sm.png'} width={160} height={100} alt={'Logo young'}/>
           <form className='form' onSubmit={handleSubmit}>
-            <input
+            <div className='frow'>
+              <h1>Ouvidoria</h1>
+              <PiInfoFill className="icon-large"  data-bs-toggle="tooltip" data-bs-placement="top" title="A Ouvidoria é um canal para receber e tratar reclamações, sugestões e elogios. Ela garante que todas as questões sejam analisadas e resolvidas de forma justa, promovendo a transparência e melhoria contínua."/>
+            </div><input
               type='text'
               className="input"
               placeholder='Assunto'
               value={subject}
               onChange={(e) => setSubject(e.target.value)}
+              required
             />
             <input
               type='text'
@@ -35,18 +50,19 @@ export default function Home() {
               placeholder='Descrição'
               value={description}
               onChange={(e) => setDescription(e.target.value)}
+              required
             />
             <div className={styles.containerReturnEmail}>
               <p className='detailP'>Caso você queira se identificar e receber um retorno individualizado, favor informar seu e-mail abaixo.</p>
               <div className={styles.emailContainer}>
                 <input
-                  type='text'
+                  type='email'
                   className="input"
                   placeholder='E-mail'
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
-                <button className='bttS' type="submit">Validar</button>
+                <button className='bttS' onClick={() => {alert('🌐 Feature 🚀')}}>Validar</button>
               </div>
             </div>
             <div className="submitContainer">
